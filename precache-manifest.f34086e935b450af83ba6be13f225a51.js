db5b452cf46ee797cf57703a98e4ea9e9818a357
GuilhermeMont <guilherme.monteiro@usermobile.xyz>
2020-03-31 18:01:18 -0300

    Updates

---
+++ precache-manifest.f34086e935b450af83ba6be13f225a51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aa4c84ca14fc6d4ec2a7c6ec0c08237",
+    "url": "/montsouz.github.io/index.html"
+  },
+  {
+    "revision": "0c7a82ce291f8953dbfe",
+    "url": "/montsouz.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6533fd30f7f9991fdf2c",
+    "url": "/montsouz.github.io/static/js/2.9342da57.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/montsouz.github.io/static/js/2.9342da57.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0c7a82ce291f8953dbfe",
+    "url": "/montsouz.github.io/static/js/main.0e458920.chunk.js"
+  },
+  {
+    "revision": "77b72a34221834efb078",
+    "url": "/montsouz.github.io/static/js/runtime-main.29f9a399.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/montsouz.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5a507640cc31fba69c1cfeb3490d3c6a.js"
+  "/montsouz.github.io/precache-manifest.f34086e935b450af83ba6be13f225a51.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/montsouz.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
