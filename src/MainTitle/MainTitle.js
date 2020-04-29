e9dc4c2faa5a2950537acdacf139ac806ba059d0
guilherme.monteiro <guilherme.monteiro@usemobile.xyz>
2020-04-29 10:06:45 -0300

    mobile ready

--- src/MainTitle/MainTitle.js
+++ src/MainTitle/MainTitle.js
@@ -4,10 +4,26 @@ import ContactInfo from './ContactInfo/ContactInfo'
 const MainTitle = ({action}) => {
 
     const originalMainTitle = "<\Hello there! I'm Guilherme Monteiro, Full-Stack developer/>";
+    const mobileText = [
+        '<Hello there!',
+        'I\'m Guilherme Monteiro,',
+        'Full-Stack developer/>'
+    ];
 
     return (
         <div>
             <div className={'typewriter'}>{originalMainTitle}</div>
+            <div className="css-typing">
+                <p>
+                    {mobileText[0]}
+                </p>
+                <p>
+                    {mobileText[1]}
+                </p>
+                <p>
+                    {mobileText[2]}
+                </p>
+            </div>
             <div className={'with-more-space'}>
                 <a className={'general-link with-more-space'} onClick={() => action()}>Know more</a>
             </div>
